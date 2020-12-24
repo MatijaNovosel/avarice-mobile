8858cb3762a625c23fa91a6e9a93cac4197ea039
Matija Novosel <mnovosel5@gmail.com>
2020-12-24 22:21:35 +0100

    Models

---
+++ finapp/lib/models/financial-change.dart
@@ -0,0 +1,37 @@
+class FinancialChange {
+  int id;
+  double amount;
+  String createdAt;
+  String description;
+  bool expense;
+  int paymentSourceId;
+  int appUserId;
+  List<int> tagIds;
+
+  @override
+  String toString() {
+    return '{amount: ${this.amount}, createdAt: ${this.createdAt}, description: ${this.description}}';
+  }
+
+  FinancialChange(
+      {this.id,
+      this.amount,
+      this.createdAt,
+      this.description,
+      this.expense,
+      this.paymentSourceId,
+      this.appUserId,
+      this.tagIds});
+
+  factory FinancialChange.fromJson(Map<String, dynamic> json) {
+    return FinancialChange(
+        amount: json['amount'] == null ? 0.0 : json['amount'].toDouble(),
+        appUserId: json['appUserId'] as int,
+        createdAt: json['createdAt'] as String,
+        description: json['description'] as String,
+        expense: json['expense'] as bool,
+        id: json['id'] as int,
+        paymentSourceId: json['paymentSourceId'] as int,
+        tagIds: json['tagIds'].cast<int>());
+  }
+}
