1aa8bda2ce24cea42722fcdb6580f70bbf58484a
Matija Novosel <mnovosel@omega-software.hr>
2020-12-24 16:04:08 +0100

    App fonts and changes

--- finapp/lib/main.dart
+++ finapp/lib/main.dart
@@ -1,64 +1,109 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(fontFamily: 'ProximaNova'),
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
+class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+        titleSpacing: 0,
+        backgroundColor: Colors.grey[850],
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          textBaseline: TextBaseline.ideographic,
+          children: [
+            Text('Finapp',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 255, 138, 0))),
+            Container(
+                margin: EdgeInsets.only(left: 3.0),
+                child: Container(
+                    margin: EdgeInsets.only(top: 5.0),
+                    child: Text('by Matija Novosel',
+                        style:
+                            TextStyle(fontSize: 13, color: Colors.grey[400])))),
+            Spacer(),
+            Container(
+                margin: EdgeInsets.only(right: 15.0), child: Icon(Icons.logout))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      drawer: Drawer(
+        child: Ink(
+            color: Colors.grey[850],
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                          width: 80,
+                          height: 80,
+                          margin: const EdgeInsets.only(right: 14),
+                          decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: new DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: AssetImage("assets/images/zhu.jpg")))),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Matija Novosel",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 255, 138, 0))),
+                          Text("mnovosel5@gmail.com",
+                              style: TextStyle(color: Colors.grey[400]))
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.access_alarm),
+                  title: Text('Pocetna stranica'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.access_alarm),
+                  title: Text('Financijske akcije'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.access_alarm),
+                  title: Text('Postavke'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            )),
       ),
     );
   }
