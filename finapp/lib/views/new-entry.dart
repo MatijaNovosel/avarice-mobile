416a70b06e4f20d5a6ba2f7dcb1ba6568a538f57
Matija Novosel <mnovosel5@gmail.com>
2020-12-29 20:49:46 +0100

    Tweaks

--- finapp/lib/views/new-entry.dart
+++ finapp/lib/views/new-entry.dart
@@ -53,207 +53,209 @@ class _NewEntryState extends State<NewEntry> {
           left: 12,
           right: 12,
         ),
-        child: Column(
-          children: [
-            ListTile(
-              leading: Padding(
-                padding: const EdgeInsets.only(
-                  top: 8,
-                ),
-                child: Icon(
-                  Icons.description_rounded,
-                  color: Colors.grey[350],
-                ),
-              ),
-              title: TextField(
-                decoration: InputDecoration(
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.grey[350],
-                    ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListTile(
+                leading: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 8,
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.grey[350],
-                    ),
-                  ),
-                  border: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.grey[350],
-                    ),
-                  ),
-                  hintText: "Entry description",
-                  isDense: true,
-                  labelText: "Description",
-                  focusColor: Colors.grey[350],
-                  fillColor: Colors.grey[350],
-                  hoverColor: Colors.grey[350],
-                  alignLabelWithHint: true,
-                  labelStyle: TextStyle(
+                  child: Icon(
+                    Icons.description_rounded,
                     color: Colors.grey[350],
                   ),
                 ),
-              ),
-            ),
-            ListTile(
-              leading: Padding(
-                padding: const EdgeInsets.only(
-                  top: 8,
+                title: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey[350],
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey[350],
+                      ),
+                    ),
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey[350],
+                      ),
+                    ),
+                    hintText: "Entry description",
+                    isDense: true,
+                    labelText: "Description",
+                    focusColor: Colors.grey[350],
+                    fillColor: Colors.grey[350],
+                    hoverColor: Colors.grey[350],
+                    alignLabelWithHint: true,
+                    labelStyle: TextStyle(
+                      color: Colors.grey[350],
+                    ),
+                  ),
                 ),
-                child: Icon(
-                  Icons.attach_money_rounded,
-                  color: Colors.grey[350],
-                ),
               ),
-              title: TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  suffix: Text("HRK"),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.grey[350],
-                    ),
+              ListTile(
+                leading: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 8,
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.grey[350],
-                    ),
-                  ),
-                  border: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.grey[350],
-                    ),
-                  ),
-                  hintText: "Entry amount",
-                  isDense: true,
-                  labelText: "Amount",
-                  focusColor: Colors.grey[350],
-                  fillColor: Colors.grey[350],
-                  hoverColor: Colors.grey[350],
-                  alignLabelWithHint: true,
-                  labelStyle: TextStyle(
+                  child: Icon(
+                    Icons.attach_money_rounded,
                     color: Colors.grey[350],
                   ),
                 ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(
-                top: 6,
-                bottom: 6,
-              ),
-              child: SwitchListTile(
-                title: const Text('Expense'),
-                value: _expense,
-                activeColor: Colors.orange,
-                onChanged: (bool value) {
-                  setState(() {
-                    _expense = !_expense;
-                  });
-                },
-                secondary: const Icon(
-                  Icons.done_all_rounded,
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 18),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  width: 1,
-                  color: Colors.grey[350],
-                ),
-              ),
-              constraints: BoxConstraints(
-                maxHeight: 300,
-              ),
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: TagEnum.keys.length,
-                itemBuilder: (context, i) {
-                  return CheckboxListTile(
-                    value: _checkboxValues[i],
-                    onChanged: (bool val) {
-                      setState(() {
-                        _checkboxValues[i] = !_checkboxValues[i];
-                        if (val)
-                          _selectedTags.add(
-                            TagEnum.keys.elementAt(
-                              i,
-                            ),
-                          );
-                        else
-                          _selectedTags.remove(
-                            TagEnum.keys.elementAt(i),
-                          );
-                      });
-                    },
-                    title: Text(
-                      parseTag(
-                        TagEnum.keys.elementAt(i),
+                title: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    suffix: Text("HRK"),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey[350],
                       ),
                     ),
-                    secondary: Icon(
-                      parseTagIcon(
-                        TagEnum.keys.elementAt(i),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey[350],
                       ),
+                    ),
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey[350],
+                      ),
+                    ),
+                    hintText: "Entry amount",
+                    isDense: true,
+                    labelText: "Amount",
+                    focusColor: Colors.grey[350],
+                    fillColor: Colors.grey[350],
+                    hoverColor: Colors.grey[350],
+                    alignLabelWithHint: true,
+                    labelStyle: TextStyle(
                       color: Colors.grey[350],
                     ),
-                    activeColor: Colors.orange,
-                  );
-                },
+                  ),
+                ),
               ),
-            ),
-            FutureBuilder<List<PaymentSource>>(
-              future: _paymentSources,
-              builder: (
-                BuildContext context,
-                AsyncSnapshot<List<PaymentSource>> snapshot,
-              ) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                    {
-                      return Center(
-                        child: SpinKitThreeBounce(
-                          color: Colors.red,
-                          size: 50.0,
+              Container(
+                margin: EdgeInsets.only(
+                  top: 6,
+                  bottom: 6,
+                ),
+                child: SwitchListTile(
+                  title: const Text('Expense'),
+                  value: _expense,
+                  activeColor: Colors.orange,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _expense = !_expense;
+                    });
+                  },
+                  secondary: const Icon(
+                    Icons.done_all_rounded,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 18),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.grey[350],
+                  ),
+                ),
+                constraints: BoxConstraints(
+                  maxHeight: 300,
+                ),
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: TagEnum.keys.length,
+                  itemBuilder: (context, i) {
+                    return CheckboxListTile(
+                      value: _checkboxValues[i],
+                      onChanged: (bool val) {
+                        setState(() {
+                          _checkboxValues[i] = !_checkboxValues[i];
+                          if (val)
+                            _selectedTags.add(
+                              TagEnum.keys.elementAt(
+                                i,
+                              ),
+                            );
+                          else
+                            _selectedTags.remove(
+                              TagEnum.keys.elementAt(i),
+                            );
+                        });
+                      },
+                      title: Text(
+                        parseTag(
+                          TagEnum.keys.elementAt(i),
                         ),
-                      );
-                    }
-                  default:
-                    {
-                      if (snapshot.hasError) {
-                        return Text('Error: ${snapshot.error}');
-                      } else {
-                        return SizedBox(
-                          height: 100,
-                          child: PageView.builder(
-                            itemCount: snapshot.data.length,
-                            onPageChanged: (int index) => setState(
-                              () {
-                                _index = index;
-                                _selectedPaymentSource = index;
-                              },
-                            ),
-                            itemBuilder: (_, i) {
-                              return Transform.scale(
-                                scale: i == _index ? 1 : 0.9,
-                                child: CurrentAmountCardWidget(
-                                  icon: Icons.account_balance_wallet,
-                                  color: Colors.orange[600],
-                                  paymentSource: snapshot.data[i],
-                                ),
-                              );
-                            },
+                      ),
+                      secondary: Icon(
+                        parseTagIcon(
+                          TagEnum.keys.elementAt(i),
+                        ),
+                        color: Colors.grey[350],
+                      ),
+                      activeColor: Colors.orange,
+                    );
+                  },
+                ),
+              ),
+              FutureBuilder<List<PaymentSource>>(
+                future: _paymentSources,
+                builder: (
+                  BuildContext context,
+                  AsyncSnapshot<List<PaymentSource>> snapshot,
+                ) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      {
+                        return Center(
+                          child: SpinKitThreeBounce(
+                            color: Colors.red,
+                            size: 50.0,
                           ),
                         );
                       }
-                    }
-                }
-              },
-            ),
-          ],
+                    default:
+                      {
+                        if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        } else {
+                          return SizedBox(
+                            height: 100,
+                            child: PageView.builder(
+                              itemCount: snapshot.data.length,
+                              onPageChanged: (int index) => setState(
+                                () {
+                                  _index = index;
+                                  _selectedPaymentSource = index;
+                                },
+                              ),
+                              itemBuilder: (_, i) {
+                                return Transform.scale(
+                                  scale: i == _index ? 1 : 0.9,
+                                  child: CurrentAmountCardWidget(
+                                    icon: Icons.account_balance_wallet,
+                                    color: Colors.orange[600],
+                                    paymentSource: snapshot.data[i],
+                                  ),
+                                );
+                              },
+                            ),
+                          );
+                        }
+                      }
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: SizedBox(
