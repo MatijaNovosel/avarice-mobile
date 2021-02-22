dcad34ffa85eaa09d4aed89927d0a3307a022555
MatijaNovosel <mnovosel5@gmail.com>
2021-02-22 23:58:08 +0100

    Scrolling fixed

--- finapp/lib/views/history.dart
+++ finapp/lib/views/history.dart
@@ -1,5 +1,4 @@
 import 'package:finapp/controllers/form-submit-controller.dart';
-import 'package:finapp/widgets/tag.dart';
 import 'package:flutter/material.dart';
 
 class History extends StatefulWidget {
@@ -18,51 +17,54 @@ class _HistoryState extends State<History> {
         right: 12,
         top: 12,
       ),
-      child: DataTable(
-        columns: <DataColumn>[
-          DataColumn(
-            label: Text(
-              'Description',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-          DataColumn(
-            label: Text(
-              'Amount',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-          DataColumn(
-            label: Expanded(
-              child: Text(
-                'Details',
-                textAlign: TextAlign.center,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: DataTable(
+          columns: <DataColumn>[
+            DataColumn(
+              label: Text(
+                'Description',
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-        ],
-        rows: <DataRow>[
-          for (var i = 0; i < 25; i++)
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('Description')),
-                DataCell(Text('250HRK')),
-                DataCell(
-                  FlatButton(
-                    onPressed: () {},
-                    child: new Icon(
-                      Icons.more_horiz_rounded,
-                      color: Colors.grey,
-                      size: 20.0,
-                    ),
-                    shape: new CircleBorder(),
-                    color: Colors.transparent,
-                  ),
-                ),
-              ],
+            DataColumn(
+              label: Text(
+                'Amount',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
             ),
-        ],
-      ), // ExpenseForm(controller: formSubmitController),
+            DataColumn(
+              label: Expanded(
+                child: Text(
+                  'Details',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+          rows: <DataRow>[
+            for (var i = 0; i < 50; i++)
+              DataRow(
+                cells: <DataCell>[
+                  DataCell(Text('Description')),
+                  DataCell(Text('250HRK')),
+                  DataCell(
+                    FlatButton(
+                      onPressed: () {},
+                      child: new Icon(
+                        Icons.more_horiz_rounded,
+                        color: Colors.grey,
+                        size: 20.0,
+                      ),
+                      shape: new CircleBorder(),
+                      color: Colors.transparent,
+                    ),
+                  ),
+                ],
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
